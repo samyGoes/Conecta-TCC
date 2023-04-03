0d8f750d04d6524ee3801f817e3e8eaa66f74335
Marcos Eric de Medeiros <55206574+MarcosErick3@users.noreply.github.com>
2023-04-03 00:16:55 -0300

    montando a area das instituicoes cadastradas

---
+++ area-adm/instituicoes-cadastradas.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/estili-instituicoes-cadastradas.css">
+    <title>ADM</title>
+</head>
+
+<body>
+    <header>
+        <h1>Instituiçóes cadastradas</h1>
+    </header>
+    <main>
+        <div class="opcoes">
+            <span>Selecionar Todos</span> <input type="checkbox" name="selectTodos" id="selectTodos"> <input type="text" placeholder="Pesquisar...">
+        </div>
+        <table>
+            <tr>
+                <th>
+                </th>
+                <th>
+                    ID
+                </th>
+                <th>
+                    Foto
+                </th>
+                <th>
+                    Nome
+                </th>
+                <th>
+                    Email
+                </th>
+                <th>
+                    Cidade
+                </th>
+                <th>
+                    UF
+                </th>
+                <th>
+                    País
+                </th>
+            </tr>
+            <tr>
+                <td>
+                    <input type="checkbox" name="select" id="select">
+                </td>
+                <td>
+                    1
+                </td>
+                <td>
+                    <img src="img/user.png" alt="">
+                </td>
+                <td>
+                    Flor do Norte
+                </td>
+                <td>
+                    marcoserick@gmail.com
+                </td>
+                <td>
+                    São Paulo
+                </td>
+                <td>
+                    SP
+                </td>
+                <td>
+                    Brasil
+                </td>
+            </tr>
+        </table>
+    </main>
+</body>
+
+</html>
