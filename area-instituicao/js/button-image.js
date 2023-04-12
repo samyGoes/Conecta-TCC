bc0f5b1db51e52829f22bd78e2ff4bc70a268c1b
RyanSantos26 <ryangustavo232@gmail.com>
2023-04-12 01:02:30 -0300

    tela de editar-perfil voluntario

---
+++ area-instituicao/js/button-image.js
@@ -0,0 +1,10 @@
+function readImage() {
+    if (this.files && this.files[0]) {
+        var file = new FileReader();
+        file.onload = function(e) {
+            document.getElementById("preview").src = e.target.result;
+        };       
+        file.readAsDataURL(this.files[0]);
+    }
+}
+document.getElementById("img").addEventListener("change", readImage, false);
--- area-voluntario/editarPerfil-voluntario-atualizado.php
+++ area-voluntario/editarPerfil-voluntario-atualizado.php
@@ -4,127 +4,96 @@
 ?>
 <!DOCTYPE html>
 <html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="../area-instituicao/css/estilo-arquivo-modelo.css">
+        <link rel="stylesheet" href="../area-voluntario/css/style-editarPerfil-voluntario.css">
+         <!-- LINK ICONES -->
+         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>Document</title>
+    </head>
+    <body>
+        
+        <!-- BARRA DE NAVEGAÇÂO -->
+        <nav class="cabecalho">     
+            <div class="logo">
+                <p> Conecta </p>
+            </div>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/estilo-editar-perfil-instituicao-atualizado.css">
-    <link rel="stylesheet" href="css/estilo-arquivo-modelo.css">
-    <!-- LINK ICONES -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
-        integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
-</head>
+            <!-- BOTÃO PRA ESCONDER E APARECER OS TÓPICOS -->
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label>
 
-<body>
-
-    <!-- BARRA DE NAVEGAÇÂO -->
-    <nav class="cabecalho">
-        <div class="logo">
-            <p> Conecta </p>
-        </div>
-
-        <!-- BOTÃO PRA ESCONDER E APARECER OS TÓPICOS -->
-        <input type="checkbox" id="check">
-        <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label>
-
-        <!-- TÓPICOS -->
-        <ul class="topicos-sessao-completa">
-            <ul class="topicos">
-                <li> <i class="fa-solid fa-house" id="topicos-icon-fixo"></i> <a href="../index.php"
-                        class="cabecalho-menu-item">Início</a></li>
-                <li> <i class="fa-solid fa-person" id="topicos-icon-fixo"></i> <a href="voluntarios.php"
-                        class="cabecalho-menu-item">voluntários</a></li>
-                <li> <i class="fa-sharp fa-solid fa-heart" id="topicos-icon-fixo"></i> <a
-                        href="../instituicoes/instituicoes.php" class="cabecalho-menu-item">instituições</a></li>
-                <li> <i class="fa-solid fa-briefcase" id="topicos-icon-fixo"></i> <a href="../vagas/vagas.php"
-                        class="cabecalho-menu-item">Vagas</a></li>
-                <li> <i class="fa fa-file-text" aria-hidden="true" id="topicos-icon-fixo"></i> <a
-                        href="../sobre-nos/sobre-nos.php" class="cabecalho-menu-item">sobre nós</a></li>
-                <li> <i class="fa-solid fa-phone" id="topicos-icon-fixo"></i> <a href="../contato/contato.php"
-                        class="cabecalho-menu-item">contato</a></li>
+            <!-- TÓPICOS -->
+            <ul class="topicos-sessao-completa">   
+                <ul class="topicos">
+                    <li> <i class="fa-solid fa-house" id="topicos-icon-fixo"></i> <a href="../index.php" class="cabecalho-menu-item">Início</a></li>
+                    <li> <i class="fa-solid fa-person" id="topicos-icon-fixo"></i> <a href="voluntarios.php" class="cabecalho-menu-item">voluntários</a></li>
+                    <li> <i class="fa-sharp fa-solid fa-heart" id="topicos-icon-fixo"></i> <a href="../instituicoes/instituicoes.php" class="cabecalho-menu-item">instituições</a></li>
+                    <li> <i class="fa-solid fa-briefcase" id="topicos-icon-fixo"></i> <a href="../vagas/vagas.php" class="cabecalho-menu-item">Vagas</a></li>
+                    <li> <i class="fa fa-file-text" aria-hidden="true" id="topicos-icon-fixo"></i> <a href="../sobre-nos/sobre-nos.php" class="cabecalho-menu-item">sobre nós</a></li>
+                    <li> <i class="fa-solid fa-phone" id="topicos-icon-fixo"></i> <a href="../contato/contato.php" class="cabecalho-menu-item">contato</a></li>  
+                </ul>
+               
+                <ul class="topicos-sessao-login">
+                    <li class="topicos-sessao-login-linha"><a href="../form-login.php" class="cabecalho-menu-item" id="cabecalho-menu-item-login">
+                        <i class="fa-solid fa-user" id="topicos-icon-fixo-dif"></i> login </a> <span id="nav-seta-sub-topicos"> 🢓 </span></i>
+                        <ul class="sub-topicos">
+                            <li> <a href="perfil-voluntario.php"> Meu Perfil </a></li>
+                            <li> <a href=""> Vagas </a> </li>
+                            <li> <a href="editar-perfil.php"> Configurações </a></li>
+                            <li> <a href="logout.php"> Sair </a></li>
+                        </ul>
+                    </li>
+                </ul>
             </ul>
-
-            <ul class="topicos-sessao-login">
-                <li class="topicos-sessao-login-linha"><a href="../form-login.php" class="cabecalho-menu-item"
-                        id="cabecalho-menu-item-login">
-                        <i class="fa-solid fa-user" id="topicos-icon-fixo-dif"></i> login </a> <span
-                        id="nav-seta-sub-topicos"> 🢓 </span></i>
-                    <ul class="sub-topicos">
-                        <li> <a href="perfil-voluntario.php"> Meu Perfil </a></li>
-                        <li> <a href=""> Vagas </a> </li>
-                        <li> <a href="editar-perfil.php"> Configurações </a></li>
-                        <li> <a href="logout.php"> Sair </a></li>
-                    </ul>
-                </li>
-            </ul>
-        </ul>
-    </nav>
+        </nav>
 
 
 
 
-    <!-- TITULO CONFIGURAÇÕES DO PERFIL -->
-    <div class="container-titulo-configuracoes">
-        <h1> Configurações do Perfil </h1>
-    </div>
-
-
-
-
-    <!-- NAV LATERAL -->
-    <nav class="nav-lateral">
-        <div class="nav-lateral-sessao-um">
-            <i class="fa-solid fa-bars" id="nav-lateral-icon-lista"></i>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-pen-to-square"></i> <span class="nav-lateral-topico"> Editar Perfil
-                    </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-sharp fa-solid fa-heart"></i> <span class="nav-lateral-topico"> Cadastrar
-                        Causas </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-wrench"></i> <span class="nav-lateral-topico"> Cadastrar Habilidades
-                    </span></a>
-            </div>
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-newspaper"></i> <span class="nav-lateral-topico"> Cadastrar Vagas
-                    </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-pen-to-square"></i> <span class="nav-lateral-topico"> Editar Vagas
-                    </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-gear"></i> <span class="nav-lateral-topico"> Gerenciar Vagas
-                    </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-key"></i> <span class="nav-lateral-topico">Trocar Senha </span></a>
-            </div>
-
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-xmark" id="nav-lateral-icon-excluir"></i> <span
-                        class="nav-lateral-topico">Excluir Conta </span></a>
-            </div>
+        <!-- TITULO CONFIGURAÇÕES DO PERFIL -->
+        <div class="container-titulo-configuracoes">
+            <h1> Configurações do Perfil </h1>
         </div>
 
-        <div class="nav-lateral-sessao-dois">
-            <div class="nav-lateral-box-icon">
-                <a href=""> <i class="fa-solid fa-door-open" id="nav-lateral-icon-sair"></i> <span
-                        class="nav-lateral-topico"> Sair </span></a>
+
+
+
+        <!-- NAV LATERAL -->
+        <nav class="nav-lateral">
+            <div class="nav-lateral-sessao-um">
+                <i class="fa-solid fa-bars" id="nav-lateral-icon-lista"></i>
+
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-solid fa-pen-to-square"></i> <span class="nav-lateral-topico"> Editar Perfil </span></a>
+                </div>
+
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-sharp fa-solid fa-heart"></i> <span class="nav-lateral-topico"> Adicionar Causas </span></a>
+                </div>
+
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-solid fa-briefcase"></i> <span class="nav-lateral-topico"> Vagas </span></a>
+                </div>
+
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-solid fa-key"></i> <span class="nav-lateral-topico">Trocar Senha </span></a>
+                </div>
+
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-solid fa-xmark" id="nav-lateral-icon-excluir"></i> <span class="nav-lateral-topico">Excluir Conta </span></a>
+                </div>
             </div>
-        </div>
-    </nav>
+
+            <div class="nav-lateral-sessao-dois">
+                <div class="nav-lateral-box-icon">
+                    <a href=""> <i class="fa-solid fa-door-open" id="nav-lateral-icon-sair"></i> <span class="nav-lateral-topico"> Sair </span></a>
+                </div>
+            </div>    
+        </nav>
 
 
 
@@ -132,11 +101,12 @@
 
 
 
-    <!-- CONTEUDO  -->
-    <main class="main-conteudo">
-        <!-- COLOCAR TODO O CONTEÚDO DENTRO DESSA SESSÃO -->
-        <!-- <div class="uau"></div> -->
-        <div class="main-conteudo-container-titulo">
+        <!-- CONTEUDO  -->
+        <main class="main-conteudo">
+            <!-- COLOCAR TODO O CONTEÚDO DENTRO DESSA SESSÃO -->
+            <!-- <div class="uau"></div> -->
+
+            <div class="main-conteudo-container-titulo">
             <h1>EDITAR PERFIL</h1>
             <p>Digite as novas informações que deseja inserir</p>
         </div>
@@ -260,7 +230,7 @@
                         </div>
                         <div class="div-image">
                             <div class="image">
-                                <img src="../area-instituicao/img/userInstituicao.png" id="img" alt="user-instituição">
+                                <img src="../area-voluntario/img/userVoluntario.png" id="img" alt="user-instituição">
                             </div>
                         </div>
 
@@ -273,18 +243,16 @@
             </form>
         </div>
 
-
-    </main>
-
+        </main>
 
 
 
 
 
+        
 
 
-    <script src="js/script.js"></script>
-    <script src="../area-voluntario/js/button-image.js"></script>
-</body>
-
+        <script src="../area-instituicao/js/script.js"></script>
+        <script src="js/button-image.js"></script>
+    </body>
 </html>
