66426f8197eea1a98a82ee5091df1709cc578180
Sâmily Goes <samily.goes@etec.sp.gov.br>
2023-04-14 09:49:33 -0300

    Tela adicionar causas finalizada

--- area-voluntario/js/drop-down-botao-causas.js
+++ area-voluntario/js/drop-down-botao-causas.js
@@ -30,22 +30,3 @@ botaoCausas.addEventListener("click", function()
 
 
 
-
-// DROP DOWN NAV BAR 
-// const navSetaDrop = document.querySelector("#nav-seta-sub-topicos");
-// const navSubTopicos = document.querySelector(".sub-topicos");
-
-// navSetaDrop.addEventListener("click", function dropDown()
-// {
-//     if (navSubTopicos.style.display == "none")
-//     {
-//         navSubTopicos.style.display = "flex";
-//         navSubTopicos.style. flexDirection = "column";
-//     }
-//     else
-//     {
-//         navSubTopicos.style.display = "none";
-//     }
-// });
-
-
