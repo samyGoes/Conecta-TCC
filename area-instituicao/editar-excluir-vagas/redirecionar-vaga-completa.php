78058866f772c06a0c5f6afe50d49d1593070a9f
Ana Claudia Ferreira <anaclaudiaegaby@gmail.com>
2023-04-21 17:18:43 -0300

    .

--- area-instituicao/editar-excluir-vagas/redirecionar-vaga-completa.php
+++ area-instituicao/editar-excluir-vagas/redirecionar-vaga-completa.php
@@ -1,14 +1,20 @@
 <?php
+    //Inserindo a função global e a função verifica-logado para acessar as sessões já iniciadas
      require_once 'global.php';  
      require_once '../../auth/verifica-logado.php';
    
     try
     {
+        //Redirecionando para a vaga completa
         header('Location: vaga-completa-instituicao.php');
-    
+
+        //Guardando o id enviado pelo formulário em uma váriavel
         $id = $_POST['id'];
+        //Guardando o resultado da função que trará todos os dados da vaga em uma váriavel
         $vaga = ServicoDao::consultarVaga($id);
+        //Guardando os dados da vaga na sessão
         $_SESSION['vaga'] = $vaga;
+        //Guardando o id da vaga na sessão
         $_SESSION['codVaga']=$id;
 
     }
