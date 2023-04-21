1c505773ab24cfd959e5b6941bd20e3fe110f8ea
Marcos Eric de Medeiros <55206574+MarcosErick3@users.noreply.github.com>
2023-04-21 19:48:01 -0300

    modal requisitar

--- area-voluntario/perfil-voluntario.php
+++ area-voluntario/perfil-voluntario.php
@@ -1,6 +1,6 @@
-<?php include "../auth/verifica-logado.php";?>
-<?php 
-    require_once 'global.php'; 
+<?php include "../auth/verifica-logado.php"; ?>
+<?php
+require_once 'global.php';
 ?>
 
 <!DOCTYPE html>
@@ -20,40 +20,40 @@
 </head>
 
 <body>
-     <!-- BARRA DE NAVEGAÇÂO -->
-     <nav class="cabecalho">     
-            <div class="logo">
-                <p> Conecta </p>
-            </div>
+    <!-- BARRA DE NAVEGAÇÂO -->
+    <nav class="cabecalho">
+        <div class="logo">
+            <p> Conecta </p>
+        </div>
 
-            <!-- BOTÃO PRA ESCONDER E APARECER OS TÓPICOS -->
-            <input type="checkbox" id="check">
-            <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label>
+        <!-- BOTÃO PRA ESCONDER E APARECER OS TÓPICOS -->
+        <input type="checkbox" id="check">
+        <label for="check" class="checkbtn"> <i class="fas fa-bars"></i> </label>
 
-            <!-- TÓPICOS -->
-            <ul class="topicos-sessao-completa">   
-                <ul class="topicos">
-                    <li> <i class="fa-solid fa-house" id="topicos-icon-fixo"></i> <a href="../index.php" class="cabecalho-menu-item">Início</a></li>
-                    <li> <i class="fa-solid fa-person" id="topicos-icon-fixo"></i> <a href="../voluntarios/voluntarios.php" class="cabecalho-menu-item">voluntários</a></li>
-                    <li> <i class="fa-sharp fa-solid fa-heart" id="topicos-icon-fixo"></i> <a href="../instituicoes/instituicoes.php" class="cabecalho-menu-item">instituições</a></li>
-                    <li> <i class="fa-solid fa-briefcase" id="topicos-icon-fixo"></i> <a href="../vagas/vagas.php" class="cabecalho-menu-item">Vagas</a></li>
-                    <li> <i class="fa fa-file-text" aria-hidden="true" id="topicos-icon-fixo"></i> <a href="../sobre-nos/sobre-nos.php" class="cabecalho-menu-item">sobre nós</a></li>
-                    <li> <i class="fa-solid fa-phone" id="topicos-icon-fixo"></i> <a href="../contato/contato.php" class="cabecalho-menu-item">contato</a></li>  
-                </ul>
-               
-                <ul class="topicos-sessao-login">
-                    <li class="topicos-sessao-login-linha"><a href="../form-login.php" class="cabecalho-menu-item" id="cabecalho-menu-item-login">
-                        <i class="fa-solid fa-user" id="topicos-icon-fixo-dif"></i> login </a> <span id="nav-seta-sub-topicos"> 🢓 </span></i>
-                        <ul class="sub-topicos">
-                            <li> <a href="perfil-voluntario.php"> Meu Perfil </a></li>
-                            <li> <a href=""> Vagas </a> </li>
-                            <li> <a href="form-editar-perfil-voluntario.php"> Configurações </a></li>
-                            <li> <a href="../auth/logout.php"> Sair </a></li>
-                        </ul>
-                    </li>
-                </ul>
+        <!-- TÓPICOS -->
+        <ul class="topicos-sessao-completa">
+            <ul class="topicos">
+                <li> <i class="fa-solid fa-house" id="topicos-icon-fixo"></i> <a href="../index.php" class="cabecalho-menu-item">Início</a></li>
+                <li> <i class="fa-solid fa-person" id="topicos-icon-fixo"></i> <a href="../voluntarios/voluntarios.php" class="cabecalho-menu-item">voluntários</a></li>
+                <li> <i class="fa-sharp fa-solid fa-heart" id="topicos-icon-fixo"></i> <a href="../instituicoes/instituicoes.php" class="cabecalho-menu-item">instituições</a></li>
+                <li> <i class="fa-solid fa-briefcase" id="topicos-icon-fixo"></i> <a href="../vagas/vagas.php" class="cabecalho-menu-item">Vagas</a></li>
+                <li> <i class="fa fa-file-text" aria-hidden="true" id="topicos-icon-fixo"></i> <a href="../sobre-nos/sobre-nos.php" class="cabecalho-menu-item">sobre nós</a></li>
+                <li> <i class="fa-solid fa-phone" id="topicos-icon-fixo"></i> <a href="../contato/contato.php" class="cabecalho-menu-item">contato</a></li>
             </ul>
-        </nav>
+
+            <ul class="topicos-sessao-login">
+                <li class="topicos-sessao-login-linha"><a href="../form-login.php" class="cabecalho-menu-item" id="cabecalho-menu-item-login">
+                        <i class="fa-solid fa-user" id="topicos-icon-fixo-dif"></i> login </a> <span id="nav-seta-sub-topicos"> 🢓 </span></i>
+                    <ul class="sub-topicos">
+                        <li> <a href="perfil-voluntario.php"> Meu Perfil </a></li>
+                        <li> <a href=""> Vagas </a> </li>
+                        <li> <a href="form-editar-perfil-voluntario.php"> Configurações </a></li>
+                        <li> <a href="../auth/logout.php"> Sair </a></li>
+                    </ul>
+                </li>
+            </ul>
+        </ul>
+    </nav>
 
 
 
@@ -93,15 +93,15 @@
                         </div>
                         <p>
                             <?php
-                                echo($_SESSION['nomeUsuario']);
+                            echo ($_SESSION['nomeUsuario']);
                             ?>
                         </p>
                     </div>
 
                     <div class="dados-pessoais-2">
-                        <p class="dados">idade: <span><?php echo($_SESSION['idadeUsuario']." anos") ?></span></p>
+                        <p class="dados">idade: <span><?php echo ($_SESSION['idadeUsuario'] . " anos") ?></span></p>
                         <p class="dados">email: <span><?php echo ($_SESSION['emailUsuario']) ?></span></p>
-                        <p class="dados">telefone: <span><?php echo($_SESSION['numFoneUsuario1']) ?></span></p>
+                        <p class="dados">telefone: <span><?php echo ($_SESSION['numFoneUsuario1']) ?></span></p>
                         <p class="dados">Localidade: <span><?php echo ($_SESSION['cidadeUsuario'] . " - " . $_SESSION['estadoUsuario'] . ", " . $_SESSION['paisUsuario']); ?></span></p>
                     </div>
                 </div>
@@ -122,8 +122,8 @@
                     <div class="ocultar-sessao-desc">
                         <div class="texto">
                             <p>
-                                <?php 
-                                    echo($_SESSION['descUsuario'])
+                                <?php
+                                echo ($_SESSION['descUsuario'])
                                 ?>
                             </p>
                         </div>
@@ -253,75 +253,91 @@
 
             <!-- REQUISITAR VOLUNTÁRIO  -->
             <div class="requisitar">
-                <p> Agora que você já viu o perfil do voluntário considere requisitá-lo a uma de suas vagas.</p>
-                <button> REQUISITAR </button>
-            </div>
+                <p class="text-requisitar"> Agora que você já viu o perfil do voluntário considere requisitá-lo a uma de suas vagas.</p>
+                <button id="requisitar"> REQUISITAR </button>
 
-
+                <!-- Modal -->
+                <div id="meuModal" class="modal">
+                    <div class="modal-conteudo">
+                        <span class="fechar">&times;</span>
+                        <h1 class="modal-titulo">Requisição de Voluntário</h1>
+                        <p class="modal-texto">Selecione as vagas para as quais deseja atribuir a este voluntário.</p>
+                        <div class="selecione-button ">
+                            <a href="#"><button class="card-carrossel-botao" id="botao-it">Causas</button></a>
+                            <a href="#"><button class="card-carrossel-botao" id="botao-it">Habilidades</button></a>
+                        </div>
+                        <div class="footer-modal">
+                            <input type="text" name="selectVagas" id="selectVagas" placeholder="Selecione as Vagas">
+                            <button type="submit" id="meuBotao">Confirmar</button>
+                        </div>
+                    </div>
+                </div>
         </section>
     </main>
 
 
 
-     <!-- RODAPÉ -->
-     <footer>
-            <div class="container-footer">
-                <div class="footer-sessao" id="footer-sessao-1">
-                    <div class="footer-col" id="footer-col-adm">
-                        <h5>Adm</h5>
-                        <ul>
-                            <a href="login-adm.php"><li>Login</li></a>
-                        </ul>
-                    </div>
-                    <div class="footer-col">
-                        <h5>Contato</h5>
-                        <ul>
-                            <li>Tel: (11)0000-0000</li>
-                            <li>Email: conecta@gmail.com</li>
-                        </ul>
-                    </div>
+    <!-- RODAPÉ -->
+    <footer>
+        <div class="container-footer">
+            <div class="footer-sessao" id="footer-sessao-1">
+                <div class="footer-col" id="footer-col-adm">
+                    <h5>Adm</h5>
+                    <ul>
+                        <a href="login-adm.php">
+                            <li>Login</li>
+                        </a>
+                    </ul>
                 </div>
-               <div class="footer-sessao" id="footer-sessao-2">
-                    <div class="footer-col" id="footer-col-causas">
-                        <h5>Causas em Destaque</h5>
-                        <ul id="lista-causas">
-                            <div class="col">
-                                <li>- Fome</li>
-                                <li>- Criaças</li>
-                                <li>- Animais</li>
-                            </div>
-                            <div class="col" id="col-ultima">
-                                <li>- Moradores de rua</li>
-                                <li>- LGBTQIAP+</li>
-                                <li>- Mulheres</li>
-                            </div>
-                            
-                        </ul>
-                    </div>
-                    <div class="footer-col" id="footer-col-sobre">
-                        <h5>Sobre Nós</h5>
-                        <ul>
-                            <li>Nos conheca um pouco <br> mais.</li>
-                        </ul>
-                    </div>
-               </div>
-                
-            </div>
-            <div class="midias-sociais">
-                <div class="box-icon">
-                    <i class="fa-brands fa-youtube"></i>
-                </div> 
-                <div class="box-icon">
-                    <i class="fa-brands fa-instagram"></i>
-                </div> 
-                <div class="box-icon">
-                    <i class="fa-brands fa-twitter"></i>
-                </div> 
-                <div class="box-icon-git">
-                    <i class="fa-brands fa-github" id="icon-git"></i>
+                <div class="footer-col">
+                    <h5>Contato</h5>
+                    <ul>
+                        <li>Tel: (11)0000-0000</li>
+                        <li>Email: conecta@gmail.com</li>
+                    </ul>
                 </div>
             </div>
-        </footer>
+            <div class="footer-sessao" id="footer-sessao-2">
+                <div class="footer-col" id="footer-col-causas">
+                    <h5>Causas em Destaque</h5>
+                    <ul id="lista-causas">
+                        <div class="col">
+                            <li>- Fome</li>
+                            <li>- Criaças</li>
+                            <li>- Animais</li>
+                        </div>
+                        <div class="col" id="col-ultima">
+                            <li>- Moradores de rua</li>
+                            <li>- LGBTQIAP+</li>
+                            <li>- Mulheres</li>
+                        </div>
+
+                    </ul>
+                </div>
+                <div class="footer-col" id="footer-col-sobre">
+                    <h5>Sobre Nós</h5>
+                    <ul>
+                        <li>Nos conheca um pouco <br> mais.</li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+        <div class="midias-sociais">
+            <div class="box-icon">
+                <i class="fa-brands fa-youtube"></i>
+            </div>
+            <div class="box-icon">
+                <i class="fa-brands fa-instagram"></i>
+            </div>
+            <div class="box-icon">
+                <i class="fa-brands fa-twitter"></i>
+            </div>
+            <div class="box-icon-git">
+                <i class="fa-brands fa-github" id="icon-git"></i>
+            </div>
+        </div>
+    </footer>
 
 
 
@@ -330,6 +346,23 @@
     <script src="js/carrossel-vagas.js"></script>
     <script src="js/carrossel-fotos.js"></script>
     <script src="js/ocultar-sessao.js"></script>
+
+    <script>
+        // Função para abrir o modal
+        function abrirModal() {
+            document.getElementById("meuModal").style.display = "block";
+        }
+
+        // Função para fechar o modal
+        function fecharModal() {
+            document.getElementById("meuModal").style.display = "none";
+        }
+
+        // Associar o evento de clique ao botão e ao botão de fechar
+        document.getElementById("requisitar").addEventListener("click", abrirModal);
+        document.querySelector(".fechar").addEventListener("click", fecharModal);
+    </script>
+
 </body>
 
 </html>
