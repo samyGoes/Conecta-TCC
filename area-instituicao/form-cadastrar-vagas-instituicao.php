010ba8d39faf4b0e7b4bf9d4d086f87bac45fd73
Marcos Eric de Medeiros <55206574+MarcosErick3@users.noreply.github.com>
2023-05-01 18:50:46 -0300

    selects de cadastro hailidade/causas "arrumado"

--- area-instituicao/form-cadastrar-vagas-instituicao.php
+++ area-instituicao/form-cadastrar-vagas-instituicao.php
@@ -158,24 +158,23 @@ $c = $_SESSION['codUsuario'];
                     <div class="input-box">
                         <div>
                             <div class="box-filtro-causas">
-                                <label for="">Habilidades</label>
-
-                                <div class="clique-fora-h">
-                                    <div class="filtro-habilidade"> Selecione as habilidades... </div>
+                                <label for="">habilidade</label>
+                                <div class="clique-fora">
+                                    <div class="filtro-habilidade"> Selecione as habilidade... </div>
                                     <div class="box-habilidade">
                                         <?php
                                         try {
-                                            $listaHabilidade = HabilidadeServicoDao::listar();
+                                            $listahabilidade = CategoriaServicoDao::listar($_SESSION['codUsuario']);
                                         } catch (Exception $e) {
                                             echo $e->getMessage();
                                         }
                                         ?>
-                                        <?php foreach ($listaHabilidade as $habilidade) { ?>
+                                        <?php foreach ($listahabilidade as $habilidade) { ?>
                                             <div class="box-habilidade-checkbox">
                                                 <input type="checkbox" name="habilidade[]" id="habilidade" value=<?php echo
-                                                                                                                    $habilidade['codHabilidadeServico']; ?>>
+                                                                                                            $habilidade['codCategoriaServico']; ?>>
                                                 <label for="habilidade">
-                                                    <?php echo $habilidade['nomeHabilidadeServico']; ?>
+                                                    <?php echo $habilidade['nomeCategoria']; ?>
                                                 </label>
                                             </div>
 
@@ -189,7 +188,6 @@ $c = $_SESSION['codUsuario'];
                         <div>
                             <div class="box-filtro-causas">
                                 <label for="">Causas</label>
-
                                 <div class="clique-fora">
                                     <div class="filtro-causas"> Selecione as causas... </div>
                                     <div class="box-causas">
@@ -214,9 +212,7 @@ $c = $_SESSION['codUsuario'];
                                         ?>
                                     </div>
                                 </div>
-
                             </div>
-
                         </div>
 
 
@@ -298,64 +294,62 @@ $c = $_SESSION['codUsuario'];
         </div>
         </form>
     </main>
-
     <script>
-        // DROP DOWN DO BOTÃO DAS CAUSAS/HABILIDADE + MUDANDO BOTÃO DE COR
         const botaoCausas = document.querySelector(".filtro-causas");
-        const botaoHabilidade = document.querySelector(".filtro-habilidade");
         const boxCausas = document.querySelector(".box-causas");
-        const boxHabilidade = document.querySelector(".box-habilidade")
         const dropCausas = document.querySelector('.clique-fora');
-        const dropHab = document.querySelector(".clique-fora-h");
+        const botaoHabilidade = document.querySelector(".filtro-habilidade");
+        const boxhabilidade = document.querySelector(".box-habilidade");
+        const dropHabilidade = document.querySelector('.clique-fora');
 
-        // Associar o evento de clique ao botão e ao botão de fechar
-        document.getElementById("requisitar").addEventListener("click", abrirModal);
-        document.querySelector(".fechar").addEventListener("click", fecharModal);
-
-
-        // Função para abrir o modal
-        function abrirModal() {
-            document.getElementById("meuModal").style.display = "block";
-        }
-
-        // Função para fechar o modal
-        function fecharModal() {
-            document.getElementById("meuModal").style.display = "none";
-        }
-
-
-        //Botoes select
         botaoCausas.addEventListener("click", function() {
-            if (boxCausas.style.display == "none") {
+            if (boxCausas.style.display == "none" || boxCausas.style.display === "") {
                 boxCausas.style.display = "flex";
+                botaoCausas.style.backgroundColor = "#4567a5";
+                botaoCausas.style.color = "#fff";
+                botaoCausas.style.borderColor = "#4567a5";
             } else {
                 boxCausas.style.display = "none";
+                botaoCausas.style.backgroundColor = "#d6ebfd";
+                botaoCausas.style.color = "#000";
+                botaoCausas.style.borderColor = "#000";
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+        botaoHabilidade.addEventListener("click", function() {
+            if (boxhabilidade.style.display == "none" || boxhabilidade.style.display === "") {
+                boxhabilidade.style.display = "flex";
+                botaoHabilidade.style.backgroundColor = "#4567a5";
+                botaoHabilidade.style.color = "#fff";
+                botaoHabilidade.style.borderColor = "#4567a5";
+            } else {
+                boxhabilidade.style.display = "none";
+                botaoHabilidade.style.backgroundColor = "#d6ebfd";
+                botaoHabilidade.style.color = "#000";
+                botaoHabilidade.style.borderColor = "#000";
             }
         });
 
         document.addEventListener('click', function(event) {
             const target = event.target;
             if (!dropCausas.contains(target)) {
-                boxCausas.style.display = "none";
-            }
-        });
-
-        botaoHabilidade.addEventListener("click", function() {
-            if (boxHabilidade.style.display == "none") {
-                boxHabilidade.style.display = "flex";
-            } else {
-                boxHabilidade.style.display = "none";
-            }
-        });
-
-        document.addEventListener('click', function(eventh) {
-            const targetH = eventh.target;
-            if (!dropHab.contains(targetH)) {
-                boxHabilidade.style.display = "none";
+                boxhabilidade.style.display = "none";
+                botaoHabilidade.style.backgroundColor = "#d6ebfd";
+                botaoHabilidade.style.color = "#000";
+                botaoHabilidade.style.borderColor = "#000";
             }
         });
     </script>
-
+    <script src="./js/buttons-select.js"></script>
     <script src="../js/endereco-auto.js"></script>
     <script type="module" src="js/main.js"></script>
     <script src="../area-voluntario/js/button-image.js"></script>
