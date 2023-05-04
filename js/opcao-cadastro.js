809823c23bfcb04f14bc83e4ef12370befce3451
Sâmily Goes <samily.goes@etec.sp.gov.br>
2023-05-04 09:50:36 -0300

    Animação na tela opções de cadastro B)

---
+++ js/opcao-cadastro.js
@@ -0,0 +1,14 @@
+const imgCima = document.querySelector(".image-header");
+const imgBaixo = document.querySelector(".image-footer");
+
+// setTimeout(function() 
+// {
+//     imgBaixo.style.bottom = "0";
+//     imgCima.style.top = "0";
+// }, 200); // 1000 milissegundos = 1 segundo
+
+window.onload = function() 
+{
+    imgBaixo.style.bottom = "0";
+    imgCima.style.top = "0";
+}
--- opcao-cadastro.php
+++ opcao-cadastro.php
@@ -1,47 +1,45 @@
 <!DOCTYPE html>
 <html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-  <link rel="stylesheet" href="css/style-opcao-cadastro.css" />
-  <title> Opções de Cadastro </title>
-</head>
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style-opcao-cadastro.css" />
+    <title> Opções de Cadastro </title>
+  </head>
 
-<body>
+  <body>
 
- 
-
-  <main>
-     <!-- <header> -->
-     <div class="image-header"></div>
-    <!-- </header> -->
-
-    <div class="container">
-      <div class="titles">
-        <h1>PARA <label>CRIAR UMA CONTA</label> SELECIONE UMA OPÇÃO</h1>
-        <h3>Você é um voluntário ou uma instituição?</h3>
-      </div>
-
-      <div class="buttons-rows">
-        <a href="form-voluntario.php"><button id="btn-voluntario" class="button-confirm">VOLUNTÁRIO</button></a>
-        <a href="form-instituicao.php"><button id="btn-instituicao" class="button-confirm">INSTITUIÇÃO</button></a>
-      </div>
-    
-      <div class="link">
-        <a class="voltar" href="form-login.php">Voltar para o login </a>
-      </div>
-
-    </div>
-
-    <div class="image-footer"></div>
-  </main>
-</body>
-
-<!-- <footer> -->
   
-<!-- </footer> -->
+
+    <main>
+      <div class="image-header"></div>
+
+      <div class="container">
+        <div class="titles">
+          <h1>PARA <label>CRIAR UMA CONTA</label> SELECIONE UMA OPÇÃO</h1>
+          <h3>Você é um voluntário ou uma instituição?</h3>
+        </div>
+
+        <div class="buttons-rows">
+          <a href="form-voluntario.php"><button id="btn-voluntario" class="button-confirm">VOLUNTÁRIO</button></a>
+          <a href="form-instituicao.php"><button id="btn-instituicao" class="button-confirm">INSTITUIÇÃO</button></a>
+        </div>
+      
+        <div class="link">
+          <a class="voltar" href="form-login.php">Voltar para o login </a>
+        </div>
+
+      </div>
+
+      <div class="image-footer"></div>
+    </main>
+
+
+    
+    <script src="js/opcao-cadastro.js"></script>
+  </body>
 
 </html>
