982f1d97282ae1d03362b5b654a0ffd85296a324
Ana Claudia Ferreira <anaclaudiaegaby@gmail.com>
2023-06-15 12:47:11 -0300

    desabilitação quase pronta....

--- js/valida-voluntario.js
+++ js/valida-voluntario.js
@@ -6,6 +6,15 @@ const emailRegex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
 /*const foneRegex = /^\(\d{2}\)\s*\d{4,5}-?\d{4}$/;
 const dateRegex = /^(0?[1-9]|[12][0-9]|3[01])\/(0?[1-9]|1[0-2])\/\d{4}$/;*/
 
+// Adicione esta função para imprimir os valores dos campos no console
+function printFormData() {
+  let formData = [];
+  for (let i = 0; i < campos.length; i++) {
+    formData.push(campos[i].value);
+  }
+  console.log(formData);
+}
+
 function validateRequiredFields() {
     let allFieldsFilled = true;
     for (let i = 0; i < campos.length; i++) {
@@ -185,6 +194,10 @@ function passwordValidate() {
         return true;
     }
     }
+
+// Chame a função para imprimir os valores dos campos no console quando a página for carregada
+printFormData();
+
 
 
     formulario1.addEventListener('submit', function(event) {
